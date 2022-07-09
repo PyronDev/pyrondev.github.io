cee648fbae312a3d519f78f21f0e0b04608c415b
PyronDev <108733992+PyronDev@users.noreply.github.com>
2022-07-09 18:36:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ var menuOpen = false;
 
 async function menuClicked() {
 	if (menuOpen == false) {
-		document.querySelector(".menu").style.animation = 'menu-slide-out 200ms';
-		await sleep(201);
+		document.querySelector(".menu").style.animation = 'menu-slide-out 300ms';
+		await sleep(300);
 		document.querySelector(".menu-checkbox").src = "./images/close_menu.svg";
-		document.querySelector(".menu").style.animation = 'menu-slide-in 200ms';
+		document.querySelector(".menu").style.animation = 'menu-slide-in 300ms';
 		menuOpen = true;
 	} else if (menuOpen == true) {
-		document.querySelector(".menu").style.animation = 'menu-slide-out 200ms';
-		await sleep(201);
+		document.querySelector(".menu").style.animation = 'menu-slide-out 300ms';
+		await sleep(300);
 		document.querySelector(".menu-checkbox").src = "./images/menu.svg";
-		document.querySelector(".menu").style.animation = 'menu-slide-in 200ms';
+		document.querySelector(".menu").style.animation = 'menu-slide-in 300ms';
 		menuOpen = false;
 	};
-	await sleep(201);
+	await sleep(300);
 	document.querySelector(".menu").style.animation = 'none';
 };
