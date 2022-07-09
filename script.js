f3cbf4485ca76be4e08891de9d5d0a35ac0822f2
PyronDev <108733992+PyronDev@users.noreply.github.com>
2022-07-09 18:31:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+var menuOpen = false;
+
+async function menuClicked() {
+	if (menuOpen == false) {
+		document.querySelector(".menu").style.animation = 'menu-slide-out 200ms';
+		await sleep(201);
+		document.querySelector(".menu-checkbox").src = "./images/close_menu.svg";
+		document.querySelector(".menu").style.animation = 'menu-slide-in 200ms';
+		menuOpen = true;
+	} else if (menuOpen == true) {
+		document.querySelector(".menu").style.animation = 'menu-slide-out 200ms';
+		await sleep(201);
+		document.querySelector(".menu-checkbox").src = "./images/menu.svg";
+		document.querySelector(".menu").style.animation = 'menu-slide-in 200ms';
+		menuOpen = false;
+	};
+	await sleep(201);
+	document.querySelector(".menu").style.animation = 'none';
+};
