eb8b9d141242b6ad6ad349aeae60a82e7f9b63bb
RJ_Infinity <rjinfinity2004@gmail.com>
2022-07-11 13:21:11 +0100

    menu? (not my code)

--- script.js
+++ script.js
@@ -6,18 +6,24 @@ var menuOpen = false;
 
 async function menuClicked() {
 	if (menuOpen == false) {
-		document.querySelector(".menu").style.animation = 'menu-slide-out 300ms';
-		await sleep(300);
+		document.querySelector(".menu").style.animation = 'menu-rotate-out 150ms';
+		document.querySelector(".menu-content").style.animation = 'menu-slide-down 150ms';
+		await sleep(150);
+		document.querySelector(".menu-content").style.marginTop = "1px";
+		document.querySelector(".menu-content").style.ZIndex = "1";
 		document.querySelector(".menu-checkbox").src = "./images/close_menu.svg";
-		document.querySelector(".menu").style.animation = 'menu-slide-in 300ms';
+		document.querySelector(".menu").style.animation = 'menu-rotate-in 150ms';
 		menuOpen = true;
 	} else if (menuOpen == true) {
-		document.querySelector(".menu").style.animation = 'menu-slide-out 300ms';
-		await sleep(300);
+		document.querySelector(".menu").style.animation = 'menu-rotate-out 150ms';
+		document.querySelector(".menu-content").style.animation = 'menu-slide-up 150ms';
+		await sleep(150);
+		document.querySelector(".menu-content").style.marginTop = "-300px";
+		document.querySelector(".menu-content").style.ZIndex = "-1";
 		document.querySelector(".menu-checkbox").src = "./images/menu.svg";
-		document.querySelector(".menu").style.animation = 'menu-slide-in 300ms';
+		document.querySelector(".menu").style.animation = 'menu-rotate-in 150ms';
 		menuOpen = false;
 	};
-	await sleep(300);
+	await sleep(200);
 	document.querySelector(".menu").style.animation = 'none';
 };
