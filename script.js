70b54da32c4e567a604d140ba0df30154b539e6b
RJ_Infinity <rjinfinity2004@gmail.com>
2022-07-14 09:53:13 +0100

    added semicolons

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-menuCheckbox = document.getElementById("menuCheckbox")
+menuCheckbox = document.getElementById("menuCheckbox");
 menuCheckbox.checked = false;
 menuCheckbox.addEventListener('change', e => {
-	var MenuContent = document.querySelectorAll(".menu-content")
+	var MenuContent = document.querySelectorAll(".menu-content");
 	MenuContent.forEach(el => {
-		el.classList.remove("hidden")
+		el.classList.remove("hidden");
 	});
 	if(!e.target.checked){
 		var value = window.getComputedStyle(MenuContent[0]).getPropertyValue("--animation-time");
 		if (value.endsWith("ms")){
-			value = value.slice(0,-2)
+			value = value.slice(0,-2);
 		}else if(value.endsWith("s")){
-			value = value.slice(0,-1)*1000
+			value = value.slice(0,-1)*1000;
+		}else{
+			console.error("ERROR: the animation time is not in the correct format");
 		}
 		setTimeout(()=>{
 			MenuContent.forEach(el => {
-				el.classList.add("hidden")
+				el.classList.add("hidden");
 			});
 		},value);
 	}
