b75c4fefb34cf7e66a0d27291fbdef93f39bc348
RJ_Infinity <rjinfinity2004@gmail.com>
2022-07-16 14:21:50 +0100

    made the site more acsessible

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-menuCheckbox = document.getElementById("menuCheckbox");
+var menuCheckbox = document.getElementById("menuCheckbox");
 menuCheckbox.checked = false;
+var menuContent = document.getElementById("menuContent");
 menuCheckbox.addEventListener('change', e => {
-	var MenuContent = document.querySelectorAll(".menu-content");
-	MenuContent.forEach(el => {
-		el.classList.remove("hidden");
-	});
+	menuContent.classList.remove("hidden");
 	if(!e.target.checked){
-		var value = window.getComputedStyle(MenuContent[0]).getPropertyValue("--animation-time");
+		var value = window.getComputedStyle(menuContent).getPropertyValue("--animation-time");
 		if (value.endsWith("ms")){
 			value = value.slice(0,-2);
 		}else if(value.endsWith("s")){
@@ -15,9 +13,32 @@ menuCheckbox.addEventListener('change', e => {
 			console.error("ERROR: the animation time is not in the correct format");
 		}
 		setTimeout(()=>{
-			MenuContent.forEach(el => {
-				el.classList.add("hidden");
-			});
+			menuContent.classList.add("hidden");
 		},value);
 	}
+});
+menuLabel = document.querySelector(".menu[for=\"menuCheckbox\"]")
+menuLabel.addEventListener("keydown",e=>{
+	if(e.key == " " || e.key == "Enter"){
+		menuCheckbox.checked = !menuCheckbox.checked;
+		menuCheckbox.dispatchEvent(new Event("change"));
+	}
+	if(e.key=="ArrowDown" && menuCheckbox.checked){
+		document.querySelector("#menuContent > a:first-child").focus()
+	}
+});
+
+[...menuContent.children].forEach(el => {
+	el.addEventListener("keydown",e=>{
+		if(e.key=="ArrowDown" && menuCheckbox.checked && e.target.nextElementSibling != null){
+			e.target.nextElementSibling.focus()
+		}
+		if(e.key=="ArrowUp" && menuCheckbox.checked){
+			if (e.target.previousElementSibling == null){
+				menuLabel.focus()
+			}else{
+				e.target.previousElementSibling.focus()
+			}
+		}
+	})
 });
